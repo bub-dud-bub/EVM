f66949add0d9385e7077ce7e694fac2a4c20b393
Real Serafim <61107480+bub-dud-bub@users.noreply.github.com>
2023-11-02 01:14:00 +0500

    Add files via upload

---
+++ lab_1_4.ino
@@ -0,0 +1,34 @@
+int var = 0;
+
+void setup() {
+  pinMode (D0, OUTPUT);
+  pinMode (D1, OUTPUT);
+  pinMode (D2, OUTPUT);
+  attachInterrupt(D3, Interrupt, FALLING)
+}
+
+void Interrupt() {
+  if (!digitalRead(D3)) {
+    var++;	
+    switch (var) {
+      case 1:
+        digitalWrite (D2, LOW);
+        digitalWrite (D0, HIGH);
+        break;
+      case 2:
+        digitalWrite (D0, LOW);
+        digitalWrite (D1, HIGH);
+        break;
+      case 3:
+        digitalWrite (D1, LOW);
+        digitalWrite (D2, HIGH);
+        break;
+    }
+    if (var >= 3) var = 0;
+    delay(100);	
+  }
+}
+
+void loop() {
+  
+}
