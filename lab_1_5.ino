56bfcb416ea0b245d3fcfc82a863985fab84ad60
Real Serafim <61107480+bub-dud-bub@users.noreply.github.com>
2023-11-02 02:32:47 +0500

    Update lab_1_5.ino

--- lab_1_5.ino
+++ lab_1_5.ino
@@ -9,24 +9,24 @@ void setup() {
 }
 
 void Interrupt() {
-    var++;	
-    switch (var) {
-      case 1:
-        digitalWrite (D2, LOW);
-        digitalWrite (D0, HIGH);
-        break;
-      case 2:
-        digitalWrite (D0, LOW);
-        digitalWrite (D1, HIGH);
-        break;
-      case 3:
-        digitalWrite (D1, LOW);
-        digitalWrite (D2, HIGH);
-        break;
-    }
-    if (var >= 3) var = 0;
-    delay(100);	
+  var++;	
+  switch (var) {
+    case 1:
+      digitalWrite (D2, LOW);
+      digitalWrite (D0, HIGH);
+      break;
+    case 2:
+      digitalWrite (D0, LOW);
+      digitalWrite (D1, HIGH);
+      break;
+    case 3:
+      digitalWrite (D1, LOW);
+      digitalWrite (D2, HIGH);
+      break;
   }
+  if (var >= 3) var = 0;
+  delay(100);	
+}
 
 void loop() {
   
